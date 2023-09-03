1bf6c20b1691cc78ac852d620db60cb064dc7fd7
seong <sngoneee@gmail.com>
2023-09-03 22:23:05 +0900

    0903

---
+++ js/scroll.js
@@ -0,0 +1,29 @@
+
+
+
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+
+  
+
+
+
+
+
+
+  // 브라우저 resize 처리
+  window.addEventListener("resize", function() {
+    $(window).scrollTop(0);
+  })
+
+
+
+
+
+
+  })
+
+
+
+});
