9ebf99124e2f1190d2cbbec8d865fd6f7f64892e
seong <sngoneee@gmail.com>
2023-09-28 23:20:38 +0900

    footer slick img responsive

--- js/index.js
+++ js/index.js
@@ -259,10 +259,10 @@
 
 
   // 브라우저 resize 처리
-  $(window).scroll(function() {
-    window.addEventListener("resize", function() {
-      $(window).scrollTop(0);
-    })
-  })
+  // $(window).scroll(function() {
+  //   window.addEventListener("resize", function() {
+  //     $(window).scrollTop(0);
+  //   })
+  // })
 
 
